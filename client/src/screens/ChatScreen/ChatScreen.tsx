800ad8f69f316b00c4b3f871b2175548cabfe7f6
MalikMateen <54246647+MalikMateen@users.noreply.github.com>
2024-04-20 00:40:52 +0500

    [PLAT-0005] typing feature added in group

--- client/src/screens/ChatScreen/ChatScreen.tsx
+++ client/src/screens/ChatScreen/ChatScreen.tsx
@@ -1,15 +1,15 @@
 "use client";
 
-import React from "react";
+import React, { ChangeEvent } from "react";
 import io, { Socket } from "socket.io-client";
-import { Message } from "./ChatScreen.types";
+import { GroupMessage, Typing } from "./ChatScreen.types";
 
 export const ChatScreen = React.memo(function ChatScreen() {
   const [socket, setSocket] = React.useState<Socket>();
-  const [messages, setMessages] = React.useState<Message>({});
-  const [inputMessage, setInputMessage] = React.useState("");
-  const [room, setRoom] = React.useState("general1");
-  const [typingUsers, setTypingUsers] = React.useState<string[]>([]);
+  const [groupMessages, setGroupMessages] = React.useState<GroupMessage>({});
+  const [inputGroupMessage, setInputGroupMessage] = React.useState<string>("");
+  const [selectedRoom, setSelectedRoom] = React.useState<string>("general1");
+  const [groupTypingUsers, setGroupTypingUsers] = React.useState<Typing>({});
 
   React.useEffect(() => {
     const socketInstance = io("http://localhost:3001");
@@ -23,82 +23,130 @@ export const ChatScreen = React.memo(function ChatScreen() {
   React.useEffect(() => {
     if (!socket) return;
 
-    socket.emit("leave_room");
-    socket.emit("join_room", room);
+    socket.emit("leave_group_room");
+    socket.emit("join_group_room", selectedRoom);
 
     socket.on("receive_message", (data) => {
-      const { room, userId, message } = data;
-      setMessages((prevMessages) => {
+      const { roomId, userId, message } = data;
+      setGroupMessages((prevMessages) => {
         return {
           ...prevMessages,
-          [room]: [...(prevMessages[room] || []), { userId, message }],
+          [roomId]: [...(prevMessages[roomId] || []), { userId, message }],
+        };
+      });
+      setGroupTypingUsers((prevTypingUsers) => {
+        return {
+          ...prevTypingUsers,
+          [roomId]: prevTypingUsers[roomId]?.filter((user) => user !== userId),
         };
       });
     });
 
-    socket.on("user_joined", (userId) => {
-      alert(`${userId} Joined!`);
+    socket.on("user_joined_group", (data) => {
+      const { userId, roomId } = data;
+      alert(`${userId} Joined ${roomId}!`);
     });
 
-    socket.on("user_left", (userId) => {
-      alert(`${userId} Left!`);
+    socket.on("user_left_group", (data) => {
+      const { userId, roomId } = data;
+      alert(`${userId} Left ${roomId}!`);
     });
 
     return () => {
       socket.off("receive_message");
-      socket.off("user_joined");
-      socket.off("user_left");
+      socket.off("user_joined_group");
+      socket.off("user_left_group");
     };
-  }, [room, socket]);
+  }, [selectedRoom, socket]);
 
-  // React.useEffect(() => {
-  //   if (!socket) return;
+  React.useEffect(() => {
+    if (!socket) return;
 
-  //   return () => {
-  //     socket.off("user_typing");
-  //   };
-  // }, [inputMessage, socket, typingUsers.length]);
+    socket.on("group_typing", (data) => {
+      const { userId, roomId, message } = data;
+      if (message) {
+        const typingUsersList = groupTypingUsers[roomId];
+        if (!typingUsersList?.includes(userId)) {
+          setGroupTypingUsers((prevTypingUsers) => {
+            return {
+              ...prevTypingUsers,
+              [roomId]: [...(prevTypingUsers[roomId] || []), userId],
+            };
+          });
+        }
+      } else {
+        const typingUsersList = groupTypingUsers[roomId];
+        const updatedTypingUsersList = typingUsersList?.filter(
+          (id) => id !== userId
+        );
+        setGroupTypingUsers((prevTypingUsers: any) => {
+          return {
+            ...prevTypingUsers,
+            [roomId]: [...updatedTypingUsersList],
+          };
+        });
+      }
+    });
+
+    return () => {
+      socket.off("group_typing");
+    };
+  }, [groupTypingUsers, socket]);
 
   const handleSendMessage = React.useCallback(() => {
-    if (inputMessage.trim() !== "") {
-      socket?.emit("send_message", { room, message: inputMessage });
-      setInputMessage("");
+    if (inputGroupMessage.trim() !== "") {
+      socket?.emit("send_message_to_room", {
+        room: selectedRoom,
+        message: inputGroupMessage,
+      });
+      setInputGroupMessage("");
     }
-  }, [inputMessage, room, socket]);
+  }, [inputGroupMessage, selectedRoom, socket]);
 
-  const handleTyping = React.useCallback(() => {
-    socket?.emit("typing");
-  }, [socket]);
+  const handleInputChange = React.useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      const message = e.target.value;
+      setInputGroupMessage(message);
+      socket?.emit("typing_in_room", { room: selectedRoom, message });
+    },
+    [socket, selectedRoom]
+  );
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Chat App by Muhammad Mateen</h1>
+      <h1>
+        <b>Chat App by Muhammad Mateen</b>
+      </h1>
+      <h2>
+        <b>Your ID: </b>
+        {socket?.id}
+      </h2>
       <div>
-        <select onChange={(e) => setRoom(e.target.value)}>
+        <select onChange={(e) => setSelectedRoom(e.target.value)}>
           <option value="general1">General 1</option>
           <option value="general2">General 2</option>
           <option value="general3">General 3</option>
         </select>
         <ul>
-          {messages?.[room] &&
-            messages[room].slice(-10).map((data, index) => (
+          {groupMessages?.[selectedRoom] &&
+            groupMessages[selectedRoom].slice(-10).map((data, index) => (
               <li key={index}>
                 <b>{data.userId}:</b> {data.message}
               </li>
             ))}
         </ul>
         <div>
-          {typingUsers.map((userId) => (
-            <p className="italic" key={userId}>
-              {userId} is typing...
-            </p>
-          ))}
+          {groupTypingUsers?.[selectedRoom] &&
+            groupTypingUsers[selectedRoom].map((userId) => (
+              <p className="italic" key={userId}>
+                {userId} is typing...
+              </p>
+            ))}
         </div>
         <input
           type="text"
-          value={inputMessage}
-          onChange={(e) => setInputMessage(e.target.value)}
-          onKeyDown={handleTyping}
+          value={inputGroupMessage}
+          onChange={handleInputChange}
         />
         <button onClick={handleSendMessage}>Send</button>
       </div>
